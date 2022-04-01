dd7b90df422c7ed254af6853a1e96d287e781ea7
T.Atkins <43272999+tyronegit@users.noreply.github.com>
2022-04-01 17:07:22 -0400

    Add files via upload
    as of 4/1/2022

--- auto_sales.py
+++ auto_sales.py
@@ -1,46 +1,40 @@
-
-# from click import option
-
-
-# def menu():
-#     print("[1] Option 1")
-#     print("[2] Option 2")
-#     print("[0] Exit the program.")
-
-# menu()
-# option = int(input("\nEnter your option: "))
-
-# while option != 0:
-#     if option == 1:
-#         #do option 1 stuff
-#         print("\nOption 1 has been called.")
-#     elif option == 2:
-#         #do option 2 stuff
-#         print("\nOption 2 has been called.")
-#     else:
-#         print("\nInvalid option.")
-#     print()
-#     menu()
-#     option = int(input("\nEnter your option: "))
-
-# print("\nThanks for using this program. Goodbye.")
-
 def menu():
-    print("[1] view cars available")
-    print("[2] View customers who purchased cars")
+
+    print("\n_______________Auto Sales________________")
+    print(" ")
+    print("[1] view available cars")
+    print("[2] View customers ")
     print("[3] Place an Order")
-    print("[4] View Orders for a Customer")
+    print("[4] Delete an order")
     print("[5] Create new customer")
-    print("[6] Create new Product")
-    print("[7] Delete an order")
-    print("[8] Update customer details")
-    print("[0] Exit")    
+    print("[6] Delete a customer")
+    print("[7] Update Horse Power")       
+    print("[8] read a csv file")  
+    print("[9] view csv files")  
+    print("[0] Exit") 
 
-# print("View Products")
-from pymongo import MongoClient
+
+
+# view cars
 import datetime
-from pprint import pprint
+from tabulate import tabulate
+from pymongo import MongoClient
 
+def cars_view():    
+    try:
+        client = MongoClient()
+        db = client['tyrone_auto_sales']
+        cars = db['cars']
+        results = cars.find({},{"_id":0})
+        for result in results:
+            print(result)
+
+    except Exception as e:
+        print(e)        
+
+
+# print("Add a car")
+from pymongo import MongoClient
 client = MongoClient()
 db = client['tyrone_auto_sales']
 cars = db['cars']
@@ -63,72 +57,190 @@ def insert():
         "MSRP": msrp,
         "Date Added": datetime.datetime.now()         
         })
-        print ('\nInserted data successfully\n')
+        print ('\nInserted successfully\n')
 
     except Exception as e:
         print(e)
 
+# delete a car
+from pymongo import MongoClient
+client = MongoClient()
+db = client['tyrone_auto_sales']
+cars = db['cars']
 
+def delete():    
+    try:
+        make = input("What is the Car Make:")       
+    
+        db.cars.delete_one({       
+        "Make": make
+         })
+        print ('\nDeleted successfully\n')
+    except Exception as e:
+        print(e)
+        delete()
+
+
+# delete a customer
+from pymongo import MongoClient
+client = MongoClient()
+db = client['tyrone_auto_sales']
+cars = db['customers']
+
+def del_cust(): 
+
+    try:        
+        last_Name = input("What is the last name:")   
+    
+        db.customers.delete_one({       
+        "Last Name":last_Name
+        })        
+        print ('\nDeleted successfully\n')        
+    except Exception as e:
+        print(e)
+        
+
+#update horse power
+from pymongo import MongoClient
+client = MongoClient()
+db = client['tyrone_auto_sales']
+cars = db['cars']
+
+def update():    
+    try:
+        make = input("What is the make of the car:")   
+        engine_HP = int(input("What is the hp amount to update:"))    
+    
+        db.cars.update_one({'Make' :make},{"$set": {"Engine HP":engine_HP}})
+
+        print ('\nUpdated successfully\n')
+
+    except Exception as e:
+        print(e)
+
+# Create a new Customer
+from pymongo import MongoClient
+client = MongoClient()
+db = client['tyrone_auto_sales']
+customers = db['customers']
+
+def customers():    
+    try:
+        _id = int(input ("CustomerId:"))
+        first_Name = input("Enter First Name:")
+        last_Name = input("Enter Last Name:")
+        dob = input("Enter Date of Birth:")
+           
+        db.customers.insert_one({
+        "_id": _id,
+        "First Name": first_Name,
+        "Last Name":last_Name,  
+        "Date of Birth": dob,
+        "Date Added": datetime.datetime.now()         
+        })
+       
+        print ('\ncustomer info entered successfully\n')
+
+    except Exception as e:
+        print(e)
+        customers()
+
+# view customers
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+import datetime
+
+def view_customers():    
+    try:
+        client = MongoClient()
+        db = client['tyrone_auto_sales']
+        customers= db['customers']
+        results = customers.find({},{"_id":0})
+        for result in results:
+            # print(" ")
+            print(result)
+    except Exception as e:
+        print(e)  
+
+# Read a file
+from pymongo import MongoClient
+client = MongoClient()
+db = client['tyrone_auto_sales']
+file = db['csv_files']
+
+def file_h():    
+    try:
+        with open("file_1.csv") as file:
+                lines = file.readlines()
+                print("Extracting words...\n")
+        for line in lines:
+            word = line.split(",")
+            print(word)
+            print(type(word))       
+       
+        print ('\nfile read successfully\n')
+
+    except Exception as e:
+        print(e)
+        file_h() 
+
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+file = db['csv_files']
+def view_files():    
+    try:
+        client = MongoClient()
+        db = client['tyrone_auto_sales']
+        file = db['csv_files']
+        results = file.find({},{"_id":0})
+        for result in results:
+            print(result)
+            print(" ")
+
+    except Exception as e:
+        print(e) 
+        view_files()    
+      
+              
 menu()
 option = int(input("\nEnter your option: "))
 
 while option != 0:
     if option == 1:
-        # Load data if necessary
-        # print("\nLoad Data called.")
-        from pymongo import MongoClient
-        from bson.objectid import ObjectId
-        import datetime
-        from pprint import pprint
-
-        client = MongoClient()
-        db = client['tyrone_auto_sales']
-        cars = db['cars']
-        results = cars.find({},{"_id":0})
-        for result in results:
-            pprint(result)
+        cars_view()
+       
         
     elif option == 2:
-        # # View customers
-        # print("View Customers")
-        from pymongo import MongoClient
-        import datetime
-        from pprint import pprint3
-
-        client = MongoClient()
-        db = client['tyrone_auto_sales']
-        customers = db['customers']
-        results = customers.find()
-        for result in results:
-            pprint(result)
+        # View customers
+        view_customers()
 
     elif option == 3:
-        #View customers
+        #insert car
         insert()
+        
     elif option == 4:
-        #View customers
-        print("Place an Order")
-        pass
+        #delete car
+        delete()
+   
     elif option == 5:
-        #View customers
-        print("View Orders for a Customer")  
-        pass   
+        #new customer
+        customers()
+
     elif option == 6:
-        #View customers
-        print("Create new customer")  
-        pass   
+        #Delete customer
+        del_cust()    
+        
     elif option == 7:
-        #View customers
-        print("Create new Product")
-        pass
+        #update
+        update()   
     elif option == 8:
-        #View customers
-        print("Delete an order")  
-        pass     
+        # create cvs file
+        file_h()   
+
     elif option == 9:
-        #View customers
-        print("Update customer details")  
-        pass   
+        # create cvs file
+        view_files()      
+
     else:
         print("\nInvalid option.")
         
@@ -138,31 +250,3 @@ while option != 0:
 
 print("\nThanks for using this program. Goodbye.")
 
-
-# Function to insert data into mongo db
-
-
-     
-        
-        
-
-
-
-
-
-
-
-
-
-
-    
-# 1. Load Data
-# 2. View customers
-# 3. View Products
-# 4. Place an Order
-# 5. View Orders for a Customer
-# 6. Create new customer
-# 7. Create new Product
-# 8. Delete an order
-# 9. Update customer details
-# 0. Exit
